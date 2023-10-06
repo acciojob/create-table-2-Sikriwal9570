fd064cc1cc05c40880c87b684d660e1c76a69649
Sikriwal9570 <128585704+Sikriwal9570@users.noreply.github.com>
2023-10-06 11:31:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+// script.js
 function createTable() {
-    //Write your code here
-  
+    const numRows = parseInt(prompt("Input number of rows:"));
+    const numCols = parseInt(prompt("Input number of columns:"));
+
+    if (isNaN(numRows) || isNaN(numCols)) {
+        alert("Please enter valid numbers for rows and columns.");
+        return;
+    }
+
+    const myTable = document.getElementById("myTable");
+    myTable.innerHTML = ''; // Clear any existing table content
+
+    for (let i = 0; i < numRows; i++) {
+        const row = myTable.insertRow(i);
+        for (let j = 0; j < numCols; j++) {
+            const cell = row.insertCell(j);
+            cell.textContent = `Row-${i} Column-${j}`;
+        }
+    }
 }
